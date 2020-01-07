c6f2b46a9ec82b4c5b1fb7131ed82488c525fcbb
danielferreiradf <danielferreiradk@gmail.com>
2020-01-07 08:26:37 -0300

    added global style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+
+import GlobalStyle from "./styles/global";
 import Routes from "./Routes";
 import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Header />
       <Routes />
     </BrowserRouter>
