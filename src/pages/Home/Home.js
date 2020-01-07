66ee41e5de974e2805bc83af915f6b4f5abd313c
danielferreiradf <danielferreiradk@gmail.com>
2020-01-07 09:52:57 -0300

    added styles to home component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,106 @@
 import React from "react";
+import { MdAddShoppingCart } from "react-icons/md";
+import { ProductList } from "./styles";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis 1</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis 1</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis 1</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis 1</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis 1</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-nike-downshifter-9-masculino/26/HZM-1276-026/HZM-1276-026_zoom1.jpg"
+          alt="Tenis"
+        />
+        <strong>Tenis 1</strong>
+        <span>R$ 129.90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+          </div>
+          <span>adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
   );
 };
 
